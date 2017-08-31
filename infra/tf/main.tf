87fa4dc75d9d4bc81a604dd3848105d6526943f8
omnilinguist <omnilinguist@users.noreply.github.com>
2017-08-31 11:02:32 -0700

    parameterize s3 bucket by fabric (#17)

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dd-service-tf-state-staging"
+    bucket = "dd-service-tf-state-{{aws_account}}"
     key    = "{{fabric}}/{{service}}/terraform.tfstate"
     region = "us-west-2"
   }
