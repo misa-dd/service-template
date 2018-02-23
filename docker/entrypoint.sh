163641499ff419a7281e44e36f45a4d088651767
Bernard Liang <bliang@doordash.com>
2018-02-23 02:16:09 -0800

    update backtick syntax

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Inject sensitive environment variables from doordash-secret-service.
 # TODO (service owner) replace "service-template" with service name as registered in doordash-secret-service
-eval `doordash-secret get --service service-template`
+eval $(doordash-secret get --service service-template)
 
 export INSTANCE_LOCAL_IP=`curl -s 169.254.169.254/latest/meta-data/local-ipv4`
 
