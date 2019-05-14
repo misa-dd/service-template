5d03402f31f20a093b7a5dceef201bffebe80edf
Carlos Herrera <carlos.herrera@doordash.com>
2019-05-14 09:07:00 -0700

    testing

--- Jenkinsfile-deploy.groovy
+++ Jenkinsfile-deploy.groovy
@@ -14,6 +14,7 @@
 
 pipeline {
   options {
+    timestamps()
     skipStagesAfterUnstable()
   }
   agent {
@@ -77,6 +78,7 @@ pipeline {
           }
           catch (err) {
             println "Timed out or Aborted! Will not deploy to prod."
+            println err
             deployToProd = false
           }
         }
