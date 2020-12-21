630f214e88e319e3d13443990496dace0377944f
marcodd <marco.chirico@doordash.com>
2020-12-21 21:16:10 +0100

    bump helm provider

--- _infra/templates/common.tf
+++ _infra/templates/common.tf
@@ -39,5 +39,5 @@ provider "helm" {
     config_path = var.k8s_config_path
   }
   install_tiller = false
-  version = "0.10.1" # Heredoc strings delimited by commas broken in 0.10.2
+  version = "2.0.1" # Heredoc strings delimited by commas broken in 0.10.2
 }
